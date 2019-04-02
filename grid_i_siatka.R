c4690885383a8442d3a76fb290d0cbbfb8372b18
Aleksandra Sarna <aleksandra.sarna97@gmail.com>
2019-04-02 16:26:02 +0200

    dodałam objaśnienia z pracowni projektowej

--- grid_i_siatka.R
+++ grid_i_siatka.R
@@ -3,34 +3,38 @@ library(sp)
 library(dplyr)
 library(raster)
 
-#punkty i geometria
+# punkty i geometria
 path = 'dane/punkty1.shp'
 punkty1 <- st_read(path)
 geom <- st_geometry(punkty1)
 punkty_proj <- st_crs(punkty1)
 plot(geom)
 
-#siatka
-
-#dlaczego nie to?       class(grd) = "sfc_POLYGON" "sfc"
+# siatka
 grd <- st_make_grid(punkty1)
 plot(grd)
 plot(geom, add = TRUE)
 
-ver1 <- st_intersects(grd, geom, sparse = TRUE)
+# od dolu od lewej do prawej kazdy wiersz
+ver1 <- st_intersects(grd, geom, sparse = TRUE) # zwraca, ktory punkt znajduje sie w danym gridzie
+
+# macierz z iloscia punktow w kolumnach i z iloscia oczek siatki w wierszach, zwraca wartosc TRUE, gdy dany punkt zawiera sie w danej komorce
 ver2 <- st_intersects(grd, geom, sparse = FALSE)
 
+# dla 6 oczka siatki najpierw sprawdzana jest jego sekwencja(1 2, bo sa 2 punkty w oczku), a nastepnie losuje sie jeden element za pomoca funkcji sample
 ver1[[6]][sample(seq_along(ver1[[6]]), size = 1)]
 
 # punkty1[c(2, 5, 6, 7, 8), ]
 
-grid <- raster(extent(punkty1))
-crs(grid) = crs(punkty1)
-# st_crs(punkty1) == crs(grid) #dlaczego FALSE skoro bralam pod uwage punkty1
-grid[] <- 0
-plot(grid)
 
-#wyswietlanie rastra (?)
+# siatka zbudowana w obrebie punktow do wizualizacji
+# grid <- raster(extent(punkty1))
+# crs(grid) = crs(punkty1)
+# st_crs(punkty1) == crs(grid) #dlaczego FALSE skoro bralam pod uwage punkty1
+# grid[] <- 0 #raster musi miec jakas wartosc
+# plot(grid)
+
+# wizualizacja ilosci punktow
 # rasteryzacja <- rasterize(punkty1, grid, field = 1, fun = "count")
 # plot(rasteryzacja)
 # rasteryzacja@data@values
