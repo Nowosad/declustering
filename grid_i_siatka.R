8101adc4bfbf9771b079a21276b64fc2edd664a8
jn <tupiszakaczki@gmail.com>
2019-04-02 15:27:40 +0200

    adds tiny improvements

--- grid_i_siatka.R
+++ grid_i_siatka.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(raster)
 
 #punkty i geometria
-path = 'C:/Users/Lenovo/Desktop/Programowanie/inzynier/dane/punkty1.shp'
+path = 'dane/punkty1.shp'
 punkty1 <- st_read(path)
 geom <- st_geometry(punkty1)
 punkty_proj <- st_crs(punkty1)
@@ -13,14 +13,24 @@ plot(geom)
 #siatka
 
 #dlaczego nie to?       class(grd) = "sfc_POLYGON" "sfc"
-#grd <- st_make_grid(punkty1)
-#plot(grd)
+grd <- st_make_grid(punkty1)
+plot(grd)
+plot(geom, add = TRUE)
 
+ver1 <- st_intersects(grd, geom, sparse = TRUE)
+ver2 <- st_intersects(grd, geom, sparse = FALSE)
+
+ver1[[6]][sample(seq_along(ver1[[6]]), size = 1)]
+
+# punkty1[c(2, 5, 6, 7, 8), ]
 
 grid <- raster(extent(punkty1))
-st_crs(punkty1) == crs(grid) #dlaczego FALSE skoro bralam pod uwage punkty1
+crs(grid) = crs(punkty1)
+# st_crs(punkty1) == crs(grid) #dlaczego FALSE skoro bralam pod uwage punkty1
+grid[] <- 0
+plot(grid)
 
 #wyswietlanie rastra (?)
-
-rasteryzacja <-  rasterize(punkty1, grid, field = 1, fun = "count")
-rasteryzacja@data@values
+# rasteryzacja <- rasterize(punkty1, grid, field = 1, fun = "count")
+# plot(rasteryzacja)
+# rasteryzacja@data@values
