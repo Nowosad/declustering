5a63c47c3e7cc2182f000590e8192c9134a19f31
Aleksandra Sarna <aleksandra.sarna97@gmail.com>
2019-04-02 15:08:27 +0200

    dodaje nowe pomysly

--- grid_i_siatka.R
+++ grid_i_siatka.R
@@ -1,12 +1,26 @@
-library('sf')
-library('sp')
-library('dplyr')
+library(sf)
+library(sp)
+library(dplyr)
+library(raster)
 
-path = "C:/Users/Lenovo/Desktop/Programowanie/inzynier/dane/punkty1.shp"
+#punkty i geometria
+path = 'C:/Users/Lenovo/Desktop/Programowanie/inzynier/dane/punkty1.shp'
 punkty1 <- st_read(path)
 geom <- st_geometry(punkty1)
+punkty_proj <- st_crs(punkty1)
 plot(geom)
 
-grd <- st_make_grid(punkty1)
-plot(grd)
-plot(geom, add = TRUE)
+#siatka
+
+#dlaczego nie to?       class(grd) = "sfc_POLYGON" "sfc"
+#grd <- st_make_grid(punkty1)
+#plot(grd)
+
+
+grid <- raster(extent(punkty1))
+st_crs(punkty1) == crs(grid) #dlaczego FALSE skoro bralam pod uwage punkty1
+
+#wyswietlanie rastra (?)
+
+rasteryzacja <-  rasterize(punkty1, grid, field = 1, fun = "count")
+rasteryzacja@data@values
