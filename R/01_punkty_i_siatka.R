fd9608d5b439a9a97b056f4e9cdb249a581cea12
Aleksandra Sarna <aleksandra.sarna97@gmail.com>
2019-04-15 23:08:00 +0200

    wprowadziłam drobne zmiany

--- R/01_punkty_i_siatka.R
+++ R/01_punkty_i_siatka.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(raster)
 
 # punkty i geometria
-path <-  'dane/punkty1.shp'
+path <-  'dane/punkty5.shp'
 punkty1 <- st_read(path)
 geom <- st_geometry(punkty1)
 punkty_proj <- st_crs(punkty1)
@@ -19,12 +19,10 @@ plot(geom, add = TRUE)
 ver1 <- st_intersects(grd, geom, sparse = TRUE) # zwraca, ktory punkt znajduje sie w danym gridzie
 
 # macierz z iloscia punktow w kolumnach i z iloscia oczek siatki w wierszach, zwraca wartosc TRUE, gdy dany punkt zawiera sie w danej komorce
-ver2 <- st_intersects(grd, geom, sparse = FALSE)
+# ver2 <- st_intersects(grd, geom, sparse = FALSE)
 
 # dla zawartosci 6 oczka siatki najpierw sprawdzana jest jego sekwencja(1 2, bo sa 2 punkty w oczku), a nastepnie losuje sie jeden element za pomoca funkcji sample
 ver1[[6]][sample(seq_along(ver1[[6]]), size = 1)]
-
-# punkty1[c(2, 5, 6, 7, 8), ]
 
 
 # siatka zbudowana w obrebie punktow do wizualizacji
@@ -52,12 +50,8 @@ is.na(ver1[[1]])
 length(ver1[[1]]) == 0
 # --------------------------------------------------------------------------
 
-
-# zamieniam integer(0) na NA w liscie ver1
-ver1[sapply(ver1, function(ver1) length(ver1)==0)] <- NA
-
 # cos tu gra (v1)
-f1 = function(input){
+f1 <-  function(input){
   for (i in seq_along(input)){
     if (length(input[[i]]) == 0){
       print(input[[i]])
@@ -72,7 +66,7 @@ f1 = function(input){
 f1(ver1)
 
 # cos tu gra (v2)
-f2 = function(input){
+f2 <-  function(input){
   for (i in seq_along(input)){
     if (length(input[[i]]) > 0){
       x <- input[[i]][sample(seq_along(input[[i]]), size = 1)]
@@ -83,3 +77,4 @@ f2 = function(input){
 }
 
 f2(ver1)
+
